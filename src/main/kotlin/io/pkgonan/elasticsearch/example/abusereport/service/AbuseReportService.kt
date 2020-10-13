b290e528b2f359a81a775a5fc04ff724be8993f1
pkgonan <pkgonan1@naver.com>
2020-10-13 17:06:35 +0900

    [ADD] Handle invalid client request exception

--- src/main/kotlin/io/pkgonan/elasticsearch/example/abusereport/service/AbuseReportService.kt
+++ src/main/kotlin/io/pkgonan/elasticsearch/example/abusereport/service/AbuseReportService.kt
@@ -2,8 +2,11 @@ package io.pkgonan.elasticsearch.example.abusereport.service
 
 import io.pkgonan.elasticsearch.example.abusereport.domain.AbuseReport
 import io.pkgonan.elasticsearch.example.abusereport.domain.AbuseReportRepository
+import org.elasticsearch.ElasticsearchStatusException
+import org.elasticsearch.rest.RestStatus
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.elasticsearch.UncategorizedElasticsearchException
 import org.springframework.stereotype.Service
 
 @Service
@@ -11,10 +14,25 @@ class AbuseReportService(
     private val repository: AbuseReportRepository
 ) {
     fun integratedSearch(text: String, page: Pageable): Page<AbuseReport> {
-        return repository.findAllByText(text, page)
+        return trySearch { repository.findAllByText(text, page) }
     }
 
     fun fieldSearch(field: String, text: String, page: Pageable): Page<AbuseReport> {
-        return repository.findByField(field, text, page)
+        return trySearch { repository.findByField(field, text, page) }
+    }
+
+    private fun trySearch(function: () -> Page<AbuseReport>): Page<AbuseReport> {
+        try {
+            return function.invoke()
+        } catch (e: UncategorizedElasticsearchException) {
+            val cause = e.cause
+
+            if (cause !is ElasticsearchStatusException) throw e
+
+            if (RestStatus.BAD_REQUEST == cause.status()) {
+                // TODO : Throw Business Exception
+            }
+        }
+        return Page.empty()
     }
 }
--- src/main/kotlin/io/pkgonan/elasticsearch/example/localization/service/LocalizationService.kt
+++ src/main/kotlin/io/pkgonan/elasticsearch/example/localization/service/LocalizationService.kt
@@ -1,9 +1,13 @@
 package io.pkgonan.elasticsearch.example.localization.service
 
+import io.pkgonan.elasticsearch.example.abusereport.domain.AbuseReport
 import io.pkgonan.elasticsearch.example.localization.domain.Localization
 import io.pkgonan.elasticsearch.example.localization.domain.LocalizationRepository
+import org.elasticsearch.ElasticsearchStatusException
+import org.elasticsearch.rest.RestStatus
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.elasticsearch.UncategorizedElasticsearchException
 import org.springframework.stereotype.Service
 
 @Service
@@ -11,10 +15,25 @@ class LocalizationService(
     private val repository: LocalizationRepository
 ) {
     fun integratedSearch(text: String, page: Pageable): Page<Localization> {
-        return repository.findAllByText(text, page)
+        return trySearch { repository.findAllByText(text, page) }
     }
 
     fun fieldSearch(field: String, text: String, page: Pageable): Page<Localization> {
-        return repository.findByField(field, text, page)
+        return trySearch { repository.findByField(field, text, page) }
+    }
+
+    private fun trySearch(function: () -> Page<Localization>): Page<Localization> {
+        try {
+            return function.invoke()
+        } catch (e: UncategorizedElasticsearchException) {
+            val cause = e.cause
+
+            if (cause !is ElasticsearchStatusException) throw e
+
+            if (RestStatus.BAD_REQUEST == cause.status()) {
+                // TODO : Throw Business Exception
+            }
+        }
+        return Page.empty()
     }
 }
